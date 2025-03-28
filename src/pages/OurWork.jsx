022ee33f5ed09736747fbf38a54deb0de16c188c
pratham meena <yashmeena274@gmail.com>
2025-03-28 11:54:42 +0530

    fix: add reload functionality on initial visit to OurWork page

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import HeroHeading from '../sections/our-work/HeroHeading'
 import BrandingGrid from '../sections/our-work/BrandingGrid'
 
 function OurWork() {
+  useEffect(() => {
+    const hasReloaded = sessionStorage.getItem("hasReloaded");
+  
+    if (!hasReloaded) {
+      sessionStorage.setItem("hasReloaded", "true");
+      window.location.reload();
+    }
+  }, []);
+  
   return (
     <div className='bg-black overflow-x-hidden'>
     <HeroHeading/>
