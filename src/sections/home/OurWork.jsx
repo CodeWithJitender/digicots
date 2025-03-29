93ddafe585ad40209ee88bdce1d398156cebaec1
pratham meena <yashmeena274@gmail.com>
2025-03-29 14:28:12 +0530

    feat: add VideoLikeCanvasAnimation component and integrate it into OurWork section

--- src/sections/home/OurWork.jsx
+++ src/sections/home/OurWork.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import MainHeading from "../../components/MainHeading";
 import CylindricalSlider from "../../components/OurWorkSlider";
+import VideoLikeCanvasAnimation from "../../animation/canvas/AutoPlayCanvasAnimation";
 
 function OurWork() {
   return (
     <section className="our-work w-full bg-black">
-        <CylindricalSlider />
+      <CylindricalSlider />
+        <VideoLikeCanvasAnimation
+                  imgPath={"./our_work_frame/"}
+
+        />
     </section>
   );
 }
