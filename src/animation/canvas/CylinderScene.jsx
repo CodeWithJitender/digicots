77611c5a909b5d629a100e8178e41699a30b88b3
pratham meena <yashmeena274@gmail.com>
2025-04-09 14:58:12 +0530

    fix: format directional light position for better readability in CylinderScene component

--- src/animation/canvas/CylinderScene.jsx
+++ src/animation/canvas/CylinderScene.jsx
@@ -92,7 +92,7 @@ const CylinderScene = () => {
         {/* <ambientLight intensity={0.7} /> */}
         {/* <pointLight position={[10, 10, 10]} intensity={1} />
         <pointLight position={[-10, -10, -10]} intensity={0.5} />
-        <directionalLight position={[0, 10, 5]} intensity={0.8} castShadow /> */}
+        <directionalLight position={[0,   10, 5]} intensity={0.8} castShadow /> */}
         <CylinderSceneContent />
       </Canvas>
     </div>
